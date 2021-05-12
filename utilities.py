d12a5925749c24862c8598b1166cee765554cce5
pawlo555 <spawel555@outlook.com>
2021-05-12 18:34:45 +0200

    utilities change_image update

--- utilities.py
+++ utilities.py
@@ -1,6 +1,7 @@
 import os
-from PIL import Image
+from PIL import Image, ImageOps
 import numpy as np
+import matplotlib.pyplot as plt
 
 
 def resize(shape, source_path, destination_path):
@@ -8,7 +9,7 @@ def resize(shape, source_path, destination_path):
     for image_path in os.listdir(source_path):
         image = Image.open(source_path + "/" + image_path)
         image = image.convert('RGB')
-        image.resize(shape)
+        image = image.resize(shape)
         image.save(destination_path + "/" + str(i) + ".jpg")
         i = i + 1
 
@@ -21,11 +22,12 @@ def generate_random_images(n, shape):
 def change_image(path, changes):
     """Changes image get from path, in random changes location change value between 0 and 1"""
     image = Image.open(path)
-    image = image.convert('RGB')
+    image = ImageOps.grayscale(image)
     np_image = np.array(image)
+    np_image = np_image // 128
+
     random_positions = np.random.randint(0, 50, (changes, 2))
     for x, y in random_positions:
-        print(x, y)
         np_image[x, y] = (np_image[x, y]+1) % 2
     return np_image
 
@@ -33,7 +35,8 @@ def change_image(path, changes):
 if __name__ == "__main__":
     resize((50, 50), "leaves", "ready_leaves")
     random = generate_random_images(50, (50, 50))
-    # print(random)
-    # print(random.shape)
+    plt.imshow(random[1])
+    plt.show()
     changed_image = change_image("ready_leaves/1.jpg", 50)
-    # print(changed_image)
+    plt.imshow(changed_image)
+    plt.show()
